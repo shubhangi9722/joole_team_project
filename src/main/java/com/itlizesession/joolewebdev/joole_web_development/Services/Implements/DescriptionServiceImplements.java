1861dc870c8d9bcbf7031b1b90964bf9f9b316cb
Shubhangi Sharma <ssharm21@stevens.edu>
2022-12-08 10:40:44 -0500

    project basic structure and entity classes added

---
+++ src/main/java/com/itlizesession/joolewebdev/joole_web_development/Services/Implements/DescriptionServiceImplements.java
@@ -0,0 +1,8 @@
+package com.itlizesession.joolewebdev.joole_web_development.Services.Implements;
+
+import com.itlizesession.joolewebdev.joole_web_development.Services.DescriptionService;
+
+public class DescriptionServiceImplements implements DescriptionService {
+
+
+}
---
+++ src/main/java/com/itlizesession/joolewebdev/joole_web_development/Services/Implements/TechnicalDetailServiceImplements.java
@@ -0,0 +1,7 @@
+package com.itlizesession.joolewebdev.joole_web_development.Services.Implements;
+
+import com.itlizesession.joolewebdev.joole_web_development.Services.TechnicalDetailService;
+
+public class TechnicalDetailServiceImplements implements TechnicalDetailService {
+
+}

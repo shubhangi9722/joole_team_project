e8516f7a4ad385fa7e5b93414ed58093f62bc32c
Shubhangi Sharma <ssharm21@stevens.edu>
2022-12-08 11:43:09 -0500

    changes made to Repository folder and pom.xml

--- src/main/java/com/itlizesession/joolewebdev/joole_web_development/Repositories/ProductRepository.java
+++ src/main/java/com/itlizesession/joolewebdev/joole_web_development/Repositories/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.itlizesession.joolewebdev.joole_web_development.Repositories;
 
-public class ProductRepository {
+import com.itlizesession.joolewebdev.joole_web_development.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
 }
--- src/main/java/com/itlizesession/joolewebdev/joole_web_development/Repositories/ProductTypeRepository.java
+++ src/main/java/com/itlizesession/joolewebdev/joole_web_development/Repositories/ProductTypeRepository.java
@@ -1,4 +1,8 @@
 package com.itlizesession.joolewebdev.joole_web_development.Repositories;
 
-public class ProductTypeRepository {
+import com.itlizesession.joolewebdev.joole_web_development.Entity.ProductType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductTypeRepository extends JpaRepository<ProductType, Integer> {
+
 }

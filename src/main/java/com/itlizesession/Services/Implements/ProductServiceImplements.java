0a74f68036e72ed19ff114bf6a48db499f0b01ef
Lvzoey <lzhzoey@gmail.com>
2022-12-08 22:43:06 -0500

    modify ProductService

--- src/main/java/com/itlizesession/Services/Implements/ProductServiceImplements.java
+++ src/main/java/com/itlizesession/Services/Implements/ProductServiceImplements.java
@@ -1,7 +1,16 @@
 package com.itlizesession.Services.Implements;
 
+import com.itlizesession.Entity.Product;
+import com.itlizesession.Repositories.ProductRepository;
 import com.itlizesession.Services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ProductServiceImplements implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
 
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
 }
--- src/main/java/com/itlizesession/Services/ProductService.java
+++ src/main/java/com/itlizesession/Services/ProductService.java
@@ -1,4 +1,7 @@
 package com.itlizesession.Services;
 
+import com.itlizesession.Entity.Product;
+
 public interface ProductService {
+    Product createProduct(Product product);
 }

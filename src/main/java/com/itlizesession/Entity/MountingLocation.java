f2bbcd375f55ad40e2fbdbb5b45588b114198cb6
Lvzoey <lzhzoey@gmail.com>
2022-12-12 16:04:39 -0500

    delete enum

--- src/main/java/com/itlizesession/Entity/MountingLocation.java
+++
@@ -1,8 +0,0 @@
-package com.itlizesession.Entity;
-
-/**
- Created by Zehui Lu
- */
-public enum MountingLocation {
-    Roof, Wall, FreeStanding
-}
--- src/main/java/com/itlizesession/Entity/Type.java
+++
@@ -1,8 +0,0 @@
-package com.itlizesession.Entity;
-
-/**
- Created by Zehui Lu
- */
-public enum Type {
-    Commercial, Industrial, Residential
-}

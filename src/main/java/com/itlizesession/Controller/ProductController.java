54aa15f591c3b991bd5de0543e05482167a9268b
Lvzoey <lzhzoey@gmail.com>
2022-12-14 13:46:51 -0500

    add ProductController

---
+++ src/main/java/com/itlizesession/Controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.itlizesession.Controller;
+
+import com.itlizesession.Entity.Product;
+import com.itlizesession.Entity.Description;
+import com.itlizesession.Entity.ProductType;
+import com.itlizesession.Entity.TechnicalDetail;
+import com.itlizesession.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productController")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    // Get all products
+    @GetMapping("/products")
+    public List<Product> list() { return productService.findAllProducts(); }
+
+    @PostMapping("/create")
+    public Product createProduct(@RequestParam("brand") String brand, @RequestParam("certification") String certification,
+                                 @RequestBody ProductType productType, @RequestBody TechnicalDetail technicalDetail, @RequestBody Description description) {
+        Product product1 = new Product();
+        product1.setProductBrand(brand);
+        product1.setCertification(certification);
+        product1.setProductType(productType);
+        product1.setTechnicalDetail(technicalDetail);
+        product1.setDescription(description);
+        return productService.createProduct(product1);
+    }
+
+    @GetMapping("/read/{id}")
+    public Product readProduct(@RequestParam("productId") Integer productId) {
+        return productService.getProduct(productId);
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product, @RequestParam("productId") Integer productId) {
+        productService.updateProduct(product, productId);
+        return productService.getProduct(productId);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean deleteProduct(@RequestParam("productId") Integer productId) {
+        return productService.deleteProduct(productId);
+    }
+}

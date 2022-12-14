8f7691ebb516e4c9920b0389b697f2fc76504938
Lvzoey <70291278+Lvzoey@users.noreply.github.com>
2022-12-14 12:13:43 -0500

    Add files via upload

---
+++ src/main/java/com/itlizesession/Service/Implements/UserServiceImplements.java
@@ -0,0 +1,48 @@
+package com.itlizesession.Service.Implements;
+
+import com.itlizesession.Entity.User;
+import com.itlizesession.Repository.UserRepository;
+import com.itlizesession.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImplements implements UserService {
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User saveUser(User user) {
+        Optional<User> savedUser = repository.findByEmail(user.getEmail());
+        if(savedUser.isPresent()){
+            System.out.println("Employee already exist with given email:" + user.getEmail());
+        }
+        return repository.save(user);
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return repository.findAll();
+    }
+
+    @Override
+    public User updateUser(User user, int userId) {
+        User existingUser = repository.findById(userId).orElse(null);
+
+        // asserting as these operations will take place only when its not null
+        assert existingUser != null;
+        existingUser.setUserName(user.getUserName());
+        existingUser.setPassword(user.getPassword());
+        existingUser.setUserType(user.getUserType());
+        existingUser.setEmail(user.getEmail());
+        return repository.save(existingUser);
+    }
+
+    @Override
+    public void deleteUserById(int userId) {
+        repository.deleteById(userId);
+    }
+}
